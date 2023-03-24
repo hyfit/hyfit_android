2de561f2b03898bdfdc970101d71d953f0d351cb
Yujin-Jeong88 <yjk88@hanyang.ac.kr>
2023-03-24 21:18:51 +0900

    Feat: community mypage 수정 및 추가

--- app/src/main/java/com/example/hyfit_android/community/MyPageFragment.kt
+++ app/src/main/java/com/example/hyfit_android/community/MyPageFragment.kt
@@ -1,7 +1,28 @@
 package com.example.hyfit_android.community
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.hyfit_android.databinding.FragmentMyPageBinding
 
 
 class MyPageFragment: Fragment() {
+
+    lateinit var binding: FragmentMyPageBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMyPageBinding.inflate(inflater, container, false)
+
+        binding.backBtn.setOnClickListener {
+            (activity as com.example.hyfit_android.MainActivity).replaceFragment(CommunityFragment())
+
+        }
+        return binding.root
+    }
 }
